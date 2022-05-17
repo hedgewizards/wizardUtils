dffcca633c0c679177b3ca6c8fc9275e0cebdb64
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2022-05-16 22:28:58 -0500

    added some editor scripts

---
+++ Editor/Keybinds/KeyPressedEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace WizardUtils
+{
+    public class KeyPressedEventArgs : EventArgs
+    {
+        Event e;
+
+        public KeyCode KeyCode => e.keyCode;
+        public bool Shift => e.shift;
+        public bool Consumed { get; private set; } = false;
+
+        public KeyPressedEventArgs(Event e)
+        {
+            this.e = e;
+        }
+
+        public void Use()
+        {
+            Consumed = true;
+        }
+    }
+}
---
+++ Editor/Keybinds/KeystrokeChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace WizardUtils
+{
+    public class KeystrokeChecker
+    {
+        public EditorWindow Window;
+
+        public Event Check()
+        {
+            if (EditorWindow.focusedWindow != SceneView.currentDrawingSceneView
+                && EditorWindow.focusedWindow != Window) return null;
+            Event e = Event.current;
+            if (e.type == EventType.KeyDown)
+            {
+                return e;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
