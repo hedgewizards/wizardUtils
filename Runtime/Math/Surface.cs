a9981e59f7f9c907b008dafdbacdba6018fd0b87
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2022-08-16 21:48:02 -0500

    Added Surface

---
+++ Runtime/Math/Surface.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WizardUtils.Math
+{
+    public struct Surface
+    {
+        public Vector3 position {get; private set;}
+        public Vector3 normal { get; private set; }
+
+        public Surface(Vector3 _position, Vector3 _normal)
+        {
+            position = _position;
+            normal = _normal;
+        }
+
+        public Vector3 GetPosition()
+        {
+            return position;
+        }
+        public Vector3 GetNormal()
+        {
+            return normal;
+        }
+    }
+
+}
