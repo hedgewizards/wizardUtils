9162f5d5332e2aebc0aa5c48a6354efd34d0943e
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2022-08-28 16:43:12 -0500

    Camera Facer

---
+++ Runtime/Tools/CameraFacer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace WizardUtils
+{
+    public class CameraFacer : MonoBehaviour
+    {
+        public Camera OverrideCamera;
+        public bool YawOnly;
+
+        Camera targetCamera => OverrideCamera != null ? OverrideCamera : Camera.main;
+
+        private void LateUpdate()
+        {
+            if (targetCamera == null) return;
+            if (YawOnly)
+            {
+                transform.rotation = Quaternion.LookRotation((targetCamera.transform.position - transform.position).Flatten(), Vector3.up);
+            }
+            else
+            {
+                transform.rotation = Quaternion.LookRotation(targetCamera.transform.position - transform.position, Vector3.up);
+            }
+        }
+    }
+}
