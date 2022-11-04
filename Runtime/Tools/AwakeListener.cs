d2e96f50df61e94add999475593bc5affb04e510
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2022-11-03 20:52:25 -0500

    AwakeListener

---
+++ Runtime/Tools/AwakeListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace WizardUtils.Tools
+{
+    public class AwakeListener : MonoBehaviour
+    {
+        public UnityEvent OnAwake;
+
+        // Use this for initialization
+        void Awake()
+        {
+            OnAwake?.Invoke();
+        }
+    }
+}
