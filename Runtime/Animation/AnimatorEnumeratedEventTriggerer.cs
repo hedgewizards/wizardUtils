4ed3bb5e072213d78cec2e879adf088053ac9f51
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2023-02-16 20:12:45 -0600

    AnimatorEnumeratedEventTriggerer
    for use with the firewatch-style mechanim organization techniques

---
+++ Runtime/Animation/AnimatorEnumeratedEventTriggerer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace WizardUtils.Animations
+{
+    public class AnimatorEnumeratedEventTriggerer : MonoBehaviour
+    {
+        public Animator Target;
+        public string EventIdParameterName;
+        public string EventTriggerParameterName;
+
+        public void PlayEnumeratedEvent(int eventId)
+        {
+            Target.SetInteger(EventIdParameterName, eventId);
+            Target.SetTrigger(EventTriggerParameterName);
+        }
+    }
+}
