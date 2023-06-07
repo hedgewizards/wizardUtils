453976936afa509e3e40c53557d59f143b782b60
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2023-06-06 22:33:53 -0500

    Circle, Sphere, and Capsule gizmos drawing with DrawLines
    I just copy pasted this code from DebugDrawHelper

--- Runtime/Extensions/GizmosHelper.cs
+++ Runtime/Extensions/GizmosHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
 
 namespace WizardUtils.Extensions
@@ -51,5 +52,83 @@ namespace WizardUtils.Extensions
             Quaternion finalRotation = Quaternion.LookRotation(Vector3.Cross(Vector3.right, up), up) * rotation;
             Gizmos.DrawWireMesh(mesh, point, finalRotation, scale);
         }
+
+        private const int CircleDefaultResolution = 16;
+
+        public static void DrawCapsule(Vector3 center, float height, float radius, int resolution = CircleDefaultResolution)
+        {
+#if UNITY_EDITOR
+            Vector3 upHeight = Vector3.up * (height - radius * 2);
+
+            DrawHalfCircle(center + upHeight / 2, Quaternion.LookRotation(Vector3.left, Vector3.down), radius, resolution);
+            DrawHalfCircle(center + upHeight / 2, Quaternion.LookRotation(Vector3.forward, Vector3.down), radius, resolution);
+            DrawCircle(center + upHeight / 2, Quaternion.LookRotation(Vector3.up, Vector3.left), radius, resolution);
+
+            DrawHalfCircle(center - upHeight / 2, Quaternion.LookRotation(Vector3.left, Vector3.up), radius, resolution);
+            DrawHalfCircle(center - upHeight / 2, Quaternion.LookRotation(Vector3.forward, Vector3.up), radius, resolution);
+            DrawCircle(center - upHeight / 2, Quaternion.LookRotation(Vector3.up, Vector3.left), radius, resolution);
+
+            Gizmos.DrawRay(new Ray(center + radius * Vector3.forward + upHeight / 2, -1 * upHeight));
+            Gizmos.DrawRay(new Ray(center + radius * Vector3.left + upHeight / 2, -1 * upHeight));
+            Gizmos.DrawRay(new Ray(center + radius * Vector3.right + upHeight / 2, -1 * upHeight));
+            Gizmos.DrawRay(new Ray(center + radius * Vector3.back + upHeight / 2, -1 * upHeight));
+#endif
+        }
+
+        public static void DrawSphere(Vector3 center, float radius, int resolution = CircleDefaultResolution)
+        {
+#if UNITY_EDITOR
+            DrawCircle(center, Quaternion.LookRotation(Vector3.up, Vector3.left), radius, resolution);
+            DrawCircle(center, Quaternion.LookRotation(Vector3.left, Vector3.up), radius, resolution);
+            DrawCircle(center, Quaternion.LookRotation(Vector3.forward, Vector3.up), radius, resolution);
+
+            var view = SceneView.currentDrawingSceneView;
+            var currentCam = Camera.current;
+            if (view != null)
+            {
+                Vector3 camForward = view.camera.transform.forward;
+                DrawCircle(center, Quaternion.LookRotation(camForward, Vector3.up), radius);
+            }
+            else if (currentCam != null)
+            {
+                Vector3 camForward = currentCam.transform.forward;
+                DrawCircle(center, Quaternion.LookRotation(camForward, Vector3.up), radius);
+            }
+#endif
+        }
+
+        public static void DrawCircle(Vector3 center, Quaternion axis, float radius, int resolution = CircleDefaultResolution)
+        {
+#if UNITY_EDITOR
+            Vector3 spoke = axis * Vector3.left * radius;
+            Vector3 pole = axis * Vector3.forward;
+            Vector3 lastPoint = center + spoke;
+            for (int n = 1; n <= resolution; n++)
+            {
+                Vector3 nextPoint = center + Quaternion.AngleAxis(n * 360f / resolution, pole) * spoke;
+
+                Gizmos.DrawLine(lastPoint, nextPoint);
+                lastPoint = nextPoint;
+            }
+#endif
+        }
+
+        public static void DrawHalfCircle(Vector3 center, Quaternion axis, float radius, int resolution = CircleDefaultResolution)
+        {
+#if UNITY_EDITOR
+            Vector3 spoke = axis * Vector3.left * radius;
+            Vector3 pole = axis * Vector3.forward;
+            Vector3 lastPoint = center + spoke;
+
+            for (int n = 1; n <= (resolution / 2); n++)
+            {
+                Vector3 nextPoint = center + Quaternion.AngleAxis(n * 360f / resolution, pole) * spoke;
+
+                Gizmos.DrawLine(lastPoint, nextPoint);
+                lastPoint = nextPoint;
+            }
+#endif
+        }
     }
+}
 }
