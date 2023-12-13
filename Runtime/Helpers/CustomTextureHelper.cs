caa34217ae076cabc7e1d04d3b30f014914cb12b
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2023-12-12 19:49:54 -0600

    added CustomTextureHelper and PathHelper from pogo

---
+++ Runtime/Helpers/CustomTextureHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WizardUtils
+{
+    public static class CustomTextureHelper
+    {
+        public static string CustomFolderPath => $"{GameManager.GameInstance.PersistentDataPath}{Path.DirectorySeparatorChar}custom";
+        public static Texture2D Load(string name, Texture2D fallbackTexture)
+        {
+            Directory.CreateDirectory(CustomFolderPath);
+            string filePath = $"{CustomFolderPath}{Path.DirectorySeparatorChar}{name}";
+
+            if (!File.Exists(filePath))
+            {
+                Debug.Log($"File not found for custom Texture @ {filePath}. using default");
+                return fallbackTexture;
+            }
+
+            byte[] rawFile = File.ReadAllBytes(filePath);
+            Texture2D loadedTexture = new Texture2D(fallbackTexture.width, fallbackTexture.height);
+            if (!ImageConversion.LoadImage(loadedTexture, rawFile))
+            {
+                Debug.Log($"Failed to load custom Texture @ {filePath}. using default");
+                return fallbackTexture;
+            }
+
+            return loadedTexture;
+        }
+    }
+}
---
+++ Runtime/Helpers/PathHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardUtils
+{
+    public static class PathHelper
+    {
+        public static string GetParentDirectory(string path)
+        {
+            int indexOfLast = path.LastIndexOf(Path.DirectorySeparatorChar);
+            if (indexOfLast == -1)
+            {
+                throw new ArgumentException("Path has no directory seperators");
+            }
+            return path[..indexOfLast];
+        }
+    }
+}
