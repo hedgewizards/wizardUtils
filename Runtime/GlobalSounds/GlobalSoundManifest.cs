92c329d84f3f85309e5d156bc6e34d521231759d
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2023-12-16 14:50:08 -0600

    GlobalSounds manifest pattern

--- Runtime/GlobalSounds/GlobalSoundManifest.cs
+++ Runtime/GlobalSounds/GlobalSoundManifest.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using WizardUtils.ManifestPattern;
 
 namespace WizardUtils.GlobalSounds
 {
     [CreateAssetMenu(fileName = "GlobalSoundManifest", menuName = "WizardUtils/GlobalSounds/GlobalSoundManifest")]
-    public class GlobalSoundManifest : ScriptableObject
+    public class GlobalSoundManifest : ScriptableObject, IDescriptorManifest<GlobalSoundDescriptor>
     {
         public GlobalSoundDescriptor[] Descriptors;
+
+        public void Add(GlobalSoundDescriptor descriptor)
+        {
+            ArrayHelper.InsertAndResize(ref Descriptors, descriptor);
+        }
+
+        public bool Contains(GlobalSoundDescriptor descriptor)
+        {
+            return Descriptors.Contains(descriptor);
+        }
+
+        public void Remove(GlobalSoundDescriptor descriptor)
+        {
+            ArrayHelper.DeleteAndResize(ref Descriptors, descriptor);
+        }
     }
 }
