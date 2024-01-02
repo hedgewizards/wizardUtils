2a4018665f57e0d270fdba6cb1c5eba050bcb6a4
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-01-01 20:03:37 -0600

    rename ILerpable.LerpTo to better match the interface name and etc

--- Runtime/Math/ILerpable.cs
+++ Runtime/Math/ILerpable.cs
@@ -8,14 +8,14 @@ namespace WizardUtils.Math
 {
     public interface ILerpable<T> where T : ILerpable<T>
     {
-        T InterpolateTo(T b, float t);
+        T LerpTo(T b, float t);
     }
 
     public static class Lerpable
     {
         public static T Lerp<T>(T a, T b, float t) where T : ILerpable<T>
         {
-            return a.InterpolateTo(b, t);
+            return a.LerpTo(b, t);
         }
     }
 }
