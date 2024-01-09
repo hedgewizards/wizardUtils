e841f29b8d81520fb3b97d0767669d13da7bbaa1
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-01-09 02:03:29 -0600

    IntExtensions for wrap / modulo!

---
+++ Runtime/Extensions/IntExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardUtils.Extensions
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// returns <paramref name="x"/> wrapped to the range [0, <paramref name="max"/>)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int PositiveModulo(this int x, int max)
+        {
+            return (x % max + max) % max;
+        }
+
+        /// <summary>
+        /// returns <paramref name="x"/> wrapped to the range [<paramref name="min"/>, <paramref name="max"/>)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int Wrap(this int x, int min, int max)
+        {
+            return (x - min).PositiveModulo(max - min) + min;
+        }
+    }
+}
