430d7d70ed06548e401f7e59a9f014a69cccead9
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-01-18 22:30:25 -0600

    clone scriptableObjects helper script

---
+++ Runtime/Extensions/ScriptableObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WizardUtils.Extensions
+{
+    public static class ScriptableObjectExtensions
+    {
+        public static T Clone<T>(this T scriptableObject) where T : ScriptableObject
+        {
+            T obj = UnityEngine.Object.Instantiate(scriptableObject);
+            obj.name = scriptableObject.name;
+
+            return obj;
+        }
+    }
+}
