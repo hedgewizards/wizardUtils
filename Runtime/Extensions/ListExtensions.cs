c7e2264c3f335e4d1017de9859c64a7837c62f4b
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-01-20 17:13:55 -0600

    ListExtensions! safe array access for lists

---
+++ Runtime/Extensions/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardUtils.Extensions
+{
+    public static class ListExtensions
+    {
+        public static bool TryGetAtIndex<T>(this List<T> self, int index, out T result)
+        {
+            if (index >= self.Count)
+            {
+                result = default;
+                return false;
+            }
+            result = self[index];
+            return true;
+        }
+
+    }
+}
