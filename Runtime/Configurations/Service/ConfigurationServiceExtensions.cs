dc46af47e2e2c059fde59690fae2e925ee39c1f8
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-01-28 14:26:06 -0600

    SpawnEventArgs ConfigurationService extension

--- Runtime/Configurations/Service/ConfigurationServiceExtensions.cs
+++ Runtime/Configurations/Service/ConfigurationServiceExtensions.cs
@@ -5,6 +5,11 @@ namespace WizardUtils.Configurations
 {
     public static class ConfigurationServiceExtensions
     {
+        public static ValueChangedEventArgs SpawnEventArgs(this IConfigurationService config, string key, string defaultValue = null)
+        {
+            return new ValueChangedEventArgs(key, defaultValue, config.Read(key, defaultValue));
+        }
+
         public static void WriteColor(this IConfigurationService config, string key, Color color, bool writeToConfig = false)
         {
             config.Write(key, ConfigHelper.SerializeColor(color), writeToConfig);
