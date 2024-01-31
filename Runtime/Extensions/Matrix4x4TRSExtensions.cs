d78dba43e262e4e1390df129ab1c6c1106e1a958
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-01-31 00:02:31 -0600

    Matri4x4TRSExtensions

---
+++ Runtime/Extensions/Matrix4x4TRSExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WizardUtils.Extensions
+{
+    public static class Matrix4x4TRSExtensions
+    {
+        public static Vector3 ExtractPosition(this Matrix4x4 trsMatrix)
+        {
+            return trsMatrix.GetColumn(3);
+        }
+
+        public static Quaternion ExtractRotation(this Matrix4x4 trsMatrix)
+        {
+            return Quaternion.LookRotation(trsMatrix.GetColumn(2), trsMatrix.GetColumn(1));
+        }
+    }
+}
