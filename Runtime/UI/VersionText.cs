1ccca553c88deef27ade33cf593bc2d4e89bef9c
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-04-27 15:57:15 -0500

    VersionText

---
+++ Runtime/UI/VersionText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace WizardUtils.UI
+{
+    public class VersionText : MonoBehaviour
+    {
+        public TextMeshProUGUI Text;
+
+        private void Awake()
+        {
+            Text.text = $"v{Application.version}";
+        }
+    }
+}
