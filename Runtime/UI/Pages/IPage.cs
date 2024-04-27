1b087cdc4e3c155b65bcf30d3149c91ce7e88c11
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-04-26 19:08:00 -0500

    navigation stack

---
+++ Runtime/UI/Pages/IPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardUtils.UI.Pages
+{
+    public interface IPage
+    {
+        public float AppearDurationSeconds { get; }
+        public float DisappearDurationSeconds { get; }
+        public void Appear(bool instant = false);
+        public void Disappear(bool instant = false);
+    }
+}
---
+++ Runtime/UI/Pages/NavigationStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WizardUtils.UI.Pages
+{
+    public class NavigationStack : MonoBehaviour
+    {
+        private Stack<IPage> PageStack;
+        private Coroutine CurrentStackAction;
+
+        public NavigationStack(IPage rootPage)
+        {
+            PageStack = new Stack<IPage>();
+            PageStack.Push(rootPage);
+            rootPage.Appear(true);
+        }
+
+        public void Push(IPage page, bool instant = false)
+        {
+            if (CurrentStackAction != null)
+            {
+                throw new InvalidOperationException($"Tried to push page '{page}' to stack while already animating. this isn't supported yet.");
+            }
+
+            if (!instant)
+            {
+                CurrentStackAction = StartCoroutine(PushAsync(page));
+                return;
+            }
+
+            if (PageStack.TryPeek(out IPage topPage))
+            {
+                topPage.Disappear(true);
+            }
+            page.Disappear(true);
+
+        }
+
+
+        private IEnumerator PushAsync(IPage newPage)
+        {
+            if (PageStack.TryPeek(out IPage topPage))
+            {
+                topPage.Disappear();
+                yield return new WaitForSecondsRealtime(topPage.DisappearDurationSeconds);
+            }
+
+            PageStack.Push(newPage);
+            newPage.Appear();
+            yield return new WaitForSecondsRealtime(newPage.AppearDurationSeconds);
+        }
+    }
+}
