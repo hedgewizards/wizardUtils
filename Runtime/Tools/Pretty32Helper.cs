f5a1deaa5cfa2b6c1a2a9e42cd924557611bbaad
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-04-28 16:42:25 -0500

    Pretty32Helper can generate random strings with 0-9 A-Z characters
    ...i skipped a few letters that might be hard to read at a glance:
    I, L, U, V (I and L similar to 1, U and V similar to eachother)

---
+++ Runtime/Tools/Pretty32Helper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WizardUtils.Tools
+{
+    public static class Pretty32Helper
+    {
+        // sorry... there's no encode/decode for this because im lazy. I just needed raw generate for lobby codes
+
+        public static string Generate(int length, char[] Atlas = null)
+        {
+            if (Atlas == null) Atlas = ReadableAtlas;
+
+            string final = "";
+
+            for(int n = 0; n < length; n++)
+            {
+                int choice = (byte)UnityEngine.Random.Range(0, 32);
+
+                try
+                {
+                    final += Atlas[choice];
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    Debug.LogError($"Failed to index byte {choice}\n" + e.ToString());
+                    final += '?';
+                }
+            }
+
+            return final;
+        }
+
+        /// <summary>
+        /// All digits, and all letters except I, O, V, and U
+        /// </summary>
+        public static char[] ReadableAtlas => new char[]
+        {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', // 0-7
+            'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', // 8-15
+            'S', 'T', 'W', 'X', 'Y', 'Z', '0', '1', // 16-23
+            '2', '3', '4', '5', '6', '7', '8', '9' // 24-31
+        };
+    }
+}
