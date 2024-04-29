801cebdde2f88f88a3b685afadbdb01abdec0f65
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-04-28 20:06:42 -0500

    Loading Screen

---
+++ Runtime/Loading/LoadingScreen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WizardUtils.LoadingScreens
+{
+    public class LoadingScreen : MonoBehaviour
+    {
+        public virtual void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public virtual void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
