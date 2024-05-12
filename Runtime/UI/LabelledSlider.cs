8ed6a0ab1857d54dab42c62e364eee12c0aec9a2
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-05-12 12:14:43 -0500

    LabelledSlider cleanup

--- Runtime/UI/LabelledSlider.cs
+++ Runtime/UI/LabelledSlider.cs
@@ -1,13 +1,12 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class LabelledSlider : MonoBehaviour
 {
-    public Text MinLabel;
-    public Text MaxLabel;
-    public Text DisplayLabel;
+    public TextMeshProUGUI DisplayLabel;
     public string LabelDisplayFormat = "N0";
 
     public Slider Slider;
@@ -28,18 +27,8 @@ public class LabelledSlider : MonoBehaviour
         Slider.onValueChanged.AddListener(onDisplayValueChanged);
     }
 
-    public void SetShowMinMaxLabels(bool showMinMaxLabels)
-    {
-        MinLabel.gameObject.SetActive(showMinMaxLabels);
-        MaxLabel.gameObject.SetActive(showMinMaxLabels);
-    }
-
     public void Initialize(float min, float max, float value, float stepSize = 0)
     {
-        MinLabel.text = min.ToString(LabelDisplayFormat);
-
-        MaxLabel.text = max.ToString(LabelDisplayFormat);
-
         StepSize = stepSize;
         if (StepSize <= 0)
         {
@@ -86,14 +75,6 @@ public class LabelledSlider : MonoBehaviour
 
     private void fixLabels()
     {
-        if (MinLabel != null)
-        {
-            MinLabel.text = Slider.minValue.ToString(LabelDisplayFormat);
-        }
-        if (MaxLabel != null)
-        {
-            MaxLabel.text = Slider.maxValue.ToString(LabelDisplayFormat);
-        }
         if (DisplayLabel != null)
         {
             DisplayLabel.text = Slider.value.ToString(LabelDisplayFormat);
