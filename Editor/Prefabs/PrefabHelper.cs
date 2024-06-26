109588796febb08cb80798bf52ba62d808ab03bb
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-06-26 02:39:28 -0500

    PrefabHelper.GameObjectIsPrefab
    an editor function that tells you if the supplied gameobject is an
    instance of the supplied prefab

---
+++ Editor/Prefabs/PrefabHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace WizardUtils.Prefabs
+{
+    public static class PrefabHelper
+    {
+        public static bool GameObjectIsPrefab(GameObject instance, GameObject prefab)
+        {
+            if (instance == prefab) return true;
+
+            // Check if the gameObject is an instance of the prefab
+            if (PrefabUtility.GetPrefabInstanceHandle(instance) == prefab)
+            {
+                return true;
+            }
+
+            // Check if the gameObject is the prefab itself
+            if (PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(instance) == AssetDatabase.GetAssetPath(prefab))
+            {
+                return true;
+            }
+
+            var prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+
+            // Check if the gameObject is the prefab open in the prefab editor
+            if (prefabStage.mode == PrefabStage.Mode.InIsolation
+                && prefabStage.assetPath == AssetDatabase.GetAssetPath(prefab)
+                && prefabStage.prefabContentsRoot == instance)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
