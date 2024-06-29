57378066fef83cd343b4ae7675e2292ec3a788a6
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-06-29 18:49:58 -0500

    QuaternionExtensions (magnitude and Approximately)

---
+++ Runtime/Math/QuaternionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace WizardUtils.Math
+{
+    public static class QuaternionExtensions
+    {
+        public static float CalculateMagnitude(this Quaternion quaternion)
+        {
+            return Mathf.Sqrt(
+                quaternion.w * quaternion.w
+                + quaternion.x * quaternion.x
+                + quaternion.y * quaternion.y
+                + quaternion.z * quaternion.z
+                );
+        }
+
+        public static bool Approximately(this Quaternion a, Quaternion b)
+        {
+            return Mathf.Approximately(a.w, b.w)
+                && Mathf.Approximately(a.x, b.x)
+                && Mathf.Approximately(a.y, b.y)
+                && Mathf.Approximately(a.z, b.z);
+        }
+    }
+}
