5975ab07351272f4b6a221d70050d3f2fb2ae657
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-10-03 23:30:22 -0500

    GetEnumerableAssetsOfType

---
+++ Editor/Tools/AssetEnumeration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace WizardUtils.Tools
+{
+    public static class AssetEnumeration
+    {
+        public static IEnumerable<T> GetEnumerableAssetsOfType<T>()
+            where T : UnityEngine.Object
+        {
+            return AssetDatabase.FindAssets($"t:{typeof(T)}")
+                .Select(id => AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(id)));
+        }
+    }
+}
