42cdffe1a8dcc448c54f53ee2dd959ff75cf05e5
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-10-20 17:37:29 -0500

    DestroyInPlayMode oneliner monobehavior
    using this on an EditorOnly tagged object that has editor-only visuals so
    they dont show up in play mode

---
+++ Runtime/Tools/DestroyInPlayMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WizardUtils.Tools
+{
+    public class DestroyInPlayMode : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
