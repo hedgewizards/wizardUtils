7f8616cd2c5f70c0bb727e822fd89c706612c702
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-11-30 00:16:43 -0600

    DescriptorManifest

---
+++ Runtime/ManifestPattern/DescriptorManifest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using WizardUtils.ManifestPattern;
+
+namespace WizardUtils
+{
+    public class DescriptorManifest<T> : ScriptableObject, IDescriptorManifest<T>
+        where T : ScriptableObject
+    {
+        public List<T> Descriptors;
+
+        public void Add(T descriptor)
+        {
+            Descriptors.Add(descriptor);
+        }
+
+        public bool Contains(T descriptor)
+        {
+            return Descriptors.Contains(descriptor);
+        }
+
+        public void Remove(T descriptor)
+        {
+            Descriptors.Remove(descriptor);
+        }
+    }
+}
