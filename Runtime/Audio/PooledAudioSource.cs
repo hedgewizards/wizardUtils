29cf1710821f2b159c74f0a458c98ee6bd8a3275
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-12-03 20:28:50 -0600

    fix PooledAudioSource wrong pitch bug

--- Runtime/Audio/PooledAudioSource.cs
+++ Runtime/Audio/PooledAudioSource.cs
@@ -41,7 +41,7 @@ namespace WizardUtils.Audio
 
             var volume = sound.VolumeRange > 0 ? UnityEngine.Random.Range(sound.Volume - sound.VolumeRange, sound.Volume + sound.VolumeRange) : sound.Volume;
             AudioSource.volume = volume;
-            var pitch = sound.PitchRange > 0 ? UnityEngine.Random.Range(sound.Pitch - sound.PitchRange, sound.Pitch + sound.PitchRange) : sound.Volume;
+            var pitch = sound.PitchRange > 0 ? UnityEngine.Random.Range(sound.Pitch - sound.PitchRange, sound.Pitch + sound.PitchRange) : sound.Pitch;
             var maxDistance = sound.MaxDistance > 0 ? sound.MaxDistance : DefaultMaxDistance;
             AudioSource.maxDistance = maxDistance;
             PlaySound(RandomHelper.FromCollection(random, sound.Clips), pitch, soundParent);
