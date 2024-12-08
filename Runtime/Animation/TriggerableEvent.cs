64dee773cec8c9f1dee830071020373bfbed1d1c
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-12-07 18:21:58 -0600

    triggerableEvent

---
+++ Runtime/Animation/TriggerableEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace WizardUtils.Animation
+{
+    public class TriggerableEvent : MonoBehaviour
+    {
+        public UnityEvent OnTriggered;
+
+        public void Trigger()
+        {
+            OnTriggered.Invoke();
+        }
+    }
+}
