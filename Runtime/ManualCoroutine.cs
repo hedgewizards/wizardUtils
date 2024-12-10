e9841a9b4413e2ca2d30c860bff466918db1b502
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2024-12-10 02:27:53 -0600

    ManualCoroutine
    run a coroutine synchronously, but without some of the bells and
    whistles like WaitForSeconds / WaitForEndOfFrame

---
+++ Runtime/ManualCoroutine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WizardUtils
+{
+    /// <summary>
+    /// A class similar to a unity coroutine, but runs when you call Think().
+    /// Only supports "yield return null" and "yield return (another IEnumerator)
+    /// </summary>
+    public class ManualCoroutine
+    {
+        private Stack<IEnumerator> Stack;
+
+        public ManualCoroutine(IEnumerator enumerator)
+        {
+            Stack = new Stack<IEnumerator>();
+            Stack.Push(enumerator);
+        }
+
+        /// <summary>
+        /// Performs the next step in the enumerator, returning FALSE if finished
+        /// </summary>
+        /// <returns>TRUE if there's another step, else false</returns>
+        public bool Think()
+        {
+            while (Stack.Count > 0)
+            {
+                var enumerator = Stack.Peek();
+                if (!enumerator.MoveNext())
+                {
+                    Stack.Pop();
+                    continue;
+                }
+
+                if (enumerator.Current is IEnumerator nestedEnumerator)
+                {
+                    Stack.Push(nestedEnumerator);
+                    continue;
+                }
+
+                if (enumerator.Current != null)
+                {
+                    throw new InvalidOperationException($"ManualCoroutine yielded unsupported type {enumerator.Current.GetType()}. Only supports yield return null D:");
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
