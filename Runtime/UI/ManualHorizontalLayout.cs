e4da9e008e1d03c970be5a27af66ef3011d3b7dc
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2025-01-25 16:37:46 -0600

    ManualHorizontalLayout
    simplified version of unity UI horizontal layout that doesn't cause a
    single frame delay when recalculating

---
+++ Runtime/UI/ManualHorizontalLayout.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WizardUtils.UI
+{
+    public class ManualHorizontalLayout : MonoBehaviour
+    {
+        public float Spacing;
+        public float Padding;
+        public bool AutoResizeSelf;
+
+        public void Recalculate()
+        {
+            float totalWidthNoPadding = 0;
+            RectTransform[] rectTransforms = new RectTransform[transform.childCount];
+            for (int n = 0; n < transform.childCount; n++)
+            {
+                rectTransforms[n] = transform.GetChild(n).GetComponent<RectTransform>();
+                totalWidthNoPadding += rectTransforms[n].rect.width;
+            }
+
+            float runningX = Padding;
+            for (int n = 0; n < rectTransforms.Length; n++)
+            {
+                rectTransforms[n].anchoredPosition = new Vector2(runningX, 0);
+                runningX += rectTransforms[n].rect.width;
+                if (n < rectTransforms.Length - 1)
+                {
+                    runningX += Spacing;
+                }
+            }
+
+            if (AutoResizeSelf)
+            {
+                runningX += Padding;
+                RectTransform rectTransform = GetComponent<RectTransform>();
+                rectTransform.sizeDelta = new Vector2(runningX, rectTransform.sizeDelta.y);
+            }
+        }
+    }
+}
