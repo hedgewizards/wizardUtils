cb97610410c566ef727976029b8b706793f95966
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2025-01-25 17:03:24 -0600

    an extension method to get textmeshproUGUI margins in a oneliner

---
+++ Runtime/UI/TextMeshProExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+
+namespace WizardUtils.UI
+{
+    public static class TextMeshProExtensions
+    {
+        public static float GetHorizontalMarginSize(this TextMeshProUGUI self)
+        {
+            return self.margin.x + self.margin.z;
+        }
+        public static float GetVerticalMarginSize(this TextMeshProUGUI self)
+        {
+            return self.margin.y + self.margin.w;
+        }
+    }
+}
