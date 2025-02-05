79cee5469c9af8129fb2335920c6d4ef1ec0b6c6
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2025-02-04 23:29:45 -0600

    AnimatorStateExtensions for FindTransition(func) and FindExitTransition

---
+++ Editor/Extensions/AnimatorStateExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.Animations;
+
+namespace WizardUtils.Extensions
+{
+    public static class AnimatorStateExtensions
+    {
+        public static AnimatorStateTransition FindExitTransition(this AnimatorState self)
+        {
+            for (int n = 0; n < self.transitions.Length; n++)
+            {
+                if (self.transitions[n].destinationState == null)
+                {
+                    return self.transitions[n];
+                }
+            }
+
+            return null;
+        }
+
+        public static AnimatorStateTransition FindTransition(this AnimatorState self, Func<AnimatorStateTransition, bool> filter)
+        {
+            for (int n = 0; n < self.transitions.Length; n++)
+            {
+                if (filter(self.transitions[n]))
+                {
+                    return self.transitions[n];
+                }
+            }
+
+            return null;
+        }
+    }
+}
