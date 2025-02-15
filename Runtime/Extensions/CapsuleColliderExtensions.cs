129fdabf44fcdbf7d21879c4b99edb17d142e897
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2025-02-14 21:14:52 -0600

    capsulecolliderextensions
    just a quick extension method for placing a capsule collider with the
    positions of both spheres

---
+++ Runtime/Extensions/CapsuleColliderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace WizardUtils.Extensions
+{
+    public static class CapsuleColliderExtensions
+    {
+        public static void SetPosition(this CapsuleCollider capsule, Vector3 top, Vector3 bottom)
+        {
+            Vector3 midPoint = (top + bottom) / 2f;
+            Vector3 direction = (bottom - top).normalized;
+            float distance = Vector3.Distance(top, bottom);
+
+            capsule.transform.SetPositionAndRotation(midPoint, Quaternion.FromToRotation(Vector3.up, direction));
+
+            // Set collider height
+            capsule.height = distance + capsule.radius * 2;
+        }
+    }
+}
