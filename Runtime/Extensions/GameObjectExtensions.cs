6e1cf4c05d8471106ce197cc04b2b1c72a861967
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2025-02-26 19:47:54 -0600

    GameObjectExtensions.GetHierarchyPath()

---
+++ Runtime/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WizardUtils
+{
+    public static class GameObjectExtensions
+    {
+        public static string GetHierarchyPath(this GameObject self)
+        {
+            if (self == null)
+                return string.Empty;
+
+            StringBuilder pathBuilder = new StringBuilder(self.name);
+            Transform parent = self.transform.parent;
+
+            while (parent != null)
+            {
+                pathBuilder.Insert(0, parent.name + "/");
+                parent = parent.parent;
+            }
+
+            return pathBuilder.ToString();
+        }
+    }
+}
