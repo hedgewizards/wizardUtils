52e0c01780890439f800f010df6bd2bd547e7fec
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2025-07-17 00:05:55 -0500

    AnimatorStringToHash converter helper window

---
+++ Runtime/Animation/AnimatorStringToHashWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WizardUtils.Animation
+{
+    public class AnimatorStringToHashWindow : EditorWindow
+    {
+        private string InputString = "";
+        private int Hash;
+
+        [MenuItem("Window/Animation/StringToHash")]
+        public static void ShowWindow()
+        {
+            var window = GetWindow<AnimatorStringToHashWindow>("String To Hash");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            string newString = EditorGUILayout.TextField("Input String", InputString);
+            if (newString != InputString)
+            {
+                InputString = newString;
+                if (!string.IsNullOrEmpty(InputString))
+                {
+                    Hash = Animator.StringToHash(newString);
+                }
+            }
+
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.IntField("Hash", Hash);
+            EditorGUI.EndDisabledGroup();
+
+            if (GUILayout.Button("Copy to Clipboard"))
+            {
+                GUIUtility.systemCopyBuffer = Hash.ToString();
+            }
+        }
+    }
+}
