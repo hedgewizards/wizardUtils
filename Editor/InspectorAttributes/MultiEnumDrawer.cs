76c448d60fa10a849969094888a74cce4849b05b
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2025-09-23 20:49:06 -0500

    new generic struct: MultiEnum
    its an enum[] but with a custom property drawer

---
+++ Editor/InspectorAttributes/MultiEnumDrawer.cs
@@ -0,0 +1,81 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Text;
+
+[CustomPropertyDrawer(typeof(WizardUtils.Tools.MultiEnum<>), true)]
+public class MultiEnumDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        => EditorGUIUtility.singleLineHeight;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        Type enumType = fieldInfo.FieldType.GetGenericArguments()[0];
+        string[] enumNames = Enum.GetNames(enumType);
+
+        SerializedProperty valuesProp = property.FindPropertyRelative("Values");
+        string displayValue = GetDisplayValue(enumType, valuesProp);
+
+        // Split rect for label + button
+        Rect labelRect = new Rect(position.x, position.y, EditorGUIUtility.labelWidth, position.height);
+        Rect buttonRect = new Rect(position.x + EditorGUIUtility.labelWidth, position.y,
+                                   position.width - EditorGUIUtility.labelWidth, position.height);
+
+        EditorGUI.LabelField(labelRect, label);
+
+        if (EditorGUI.DropdownButton(buttonRect, new GUIContent(displayValue), FocusType.Keyboard))
+        {
+            GenericMenu menu = new GenericMenu();
+            foreach (string enumName in enumNames)
+            {
+                int enumIntValue = Convert.ToInt32(Enum.Parse(enumType, enumName));
+                bool isOn = Enumerable.Range(0, valuesProp.arraySize)
+                    .Any(i => valuesProp.GetArrayElementAtIndex(i).intValue == enumIntValue);
+                menu.AddItem(new GUIContent(enumName), isOn, () =>
+                {
+                    property.serializedObject.Update();
+                    if (isOn)
+                    {
+                        for (int i = 0; i < valuesProp.arraySize; i++)
+                        {
+                            if (valuesProp.GetArrayElementAtIndex(i).intValue == enumIntValue)
+                            {
+                                valuesProp.DeleteArrayElementAtIndex(i);
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        valuesProp.InsertArrayElementAtIndex(valuesProp.arraySize);
+                        valuesProp.GetArrayElementAtIndex(valuesProp.arraySize - 1).intValue = enumIntValue;
+                    }
+                    property.serializedObject.ApplyModifiedProperties();
+                });
+            }
+            menu.ShowAsContext();
+        }
+    }
+
+    private static string GetDisplayValue(Type enumType, SerializedProperty valuesProp)
+    {
+        if (valuesProp.arraySize > 0)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < valuesProp.arraySize; i++)
+            {
+                string name = Enum.GetName(enumType, valuesProp.GetArrayElementAtIndex(i).intValue);
+                sb.Append(name);
+                if (i < valuesProp.arraySize - 1)
+                    sb.Append(", ");
+            }
+            return sb.ToString();
+        }
+        else
+        {
+            return "(None)";
+        }
+    }
+}
---
+++ Runtime/Tools/MultiEnum.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WizardUtils.Tools
+{
+    /// <summary>
+    /// an array of enum values that unity can actually implement a custom propertydrawer for
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [System.Serializable]
+    public struct MultiEnum<T> : ICollection<T>
+        where T : Enum
+    {
+        [SerializeField]
+        private T[] Values;
+
+        public int Count => Values.Count();
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            if (!Values.Contains(item))
+            {
+                ArrayHelper.InsertAndResize(ref Values, item);
+            }
+        }
+
+        public void Clear()
+        {
+            Values = new T[0];
+        }
+
+        public bool Contains(T item)
+            => Values.Contains(item);
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Values.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+            => (IEnumerator<T>)Values.GetEnumerator();
+
+        public bool Remove(T item)
+        {
+            return ArrayHelper.DeleteAndResize(ref Values, item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
