3d97aa7fa9d3c31351f72d514e911e285d8f0a8d
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2026-04-28 00:59:26 -0500

    AdvancedAudioPlayer
    added a convenience monobehavior that does nothing but trigger an
    advanced sound effect to be played

---
+++ Runtime/Audio/AdvancedAudioPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using WizardUtils.Audio;
+
+namespace WizardUtils.Runtime.Audio
+{
+    public class AdvancedAudioPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private AdvancedSoundEffect SoundEffect;
+        [SerializeField]
+        private bool Loop;
+        public bool PlayOnAwake;
+
+        private AudioLoopInstance? LoopInstance;
+
+        private void Awake()
+        {
+            if (PlayOnAwake)
+            {
+                Play();
+            }
+        }
+
+        public void Play()
+        {
+            if (SoundEffect == null) return;
+            if (Loop)
+            {
+                PlayLoop();
+            }
+            else
+            {
+                GameManager.Instance.SoundService.PlaySound(SoundEffect, transform);
+            }
+        }
+
+        public void StopLoop()
+        {
+            if (!Loop || LoopInstance == null) return;
+
+            GameManager.Instance.SoundService.StopAudioLoop(LoopInstance.Value);
+        }
+
+        private void PlayLoop()
+        {
+            LoopInstance = GameManager.Instance.SoundService.StartAudioLoop(this, transform, SoundEffect);
+        }
+    }
+}
